a0548edb63b254fac97163b36f8ad3557e07004d
Sergii Kamenskyi <serg@apple.com>
2022-03-05 21:35:52 +0200

    Stage 1 completed!

---
+++ University Admission Procedure/task/university.py
@@ -0,0 +1,4 @@
+# -----------------------------------------------STAGE 1-----------------------------------------------
+marks = list(map(lambda x: int(input()), range(3)))
+print(sum(marks) / len(marks))
+print("Congratulations, you are accepted!")
