bbd010c0894215622d4e3139a1f26e0f72073efc
Sergii Kamenskyi <serg@apple.com>
2022-03-05 21:39:51 +0200

    stage 2 completed!

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -1,4 +1,11 @@
 # -----------------------------------------------STAGE 1-----------------------------------------------
+# marks = list(map(lambda x: int(input()), range(3)))
+# print(sum(marks) / len(marks))
+# print("Congratulations, you are accepted!")
+
+# -----------------------------------------------STAGE 2-----------------------------------------------
 marks = list(map(lambda x: int(input()), range(3)))
-print(sum(marks) / len(marks))
-print("Congratulations, you are accepted!")
+mean_score = sum(marks) / len(marks)
+print(mean_score)
+print("Congratulations, you are accepted!") if mean_score >= 60 else print(
+    "We regret to inform you that we will not be able to offer you admission.")
