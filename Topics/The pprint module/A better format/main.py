86b8f7bfc498097f9652265740a1711e2e24b9d3
Sergii Kamenskyi <serg@apple.com>
2022-03-07 11:44:29 +0200

    project completed!

---
+++ Topics/The pprint module/A better format/main.py
@@ -0,0 +1,14 @@
+dogs = [
+    {
+        "name": "Max",
+        "breed": "Yorkshire",
+        "age": 1,
+        "owners": ["Susan, Camila, Paul"]
+    },
+    {
+        "name": "Duke",
+        "breed": "Bulldog",
+        "age": 4,
+        "owners": ["Thomas, David, Lucia"]
+    }
+]
